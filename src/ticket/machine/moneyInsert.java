b007bf96e278fa150ed2f46a9756b6b612ee00a2
Paweł Kózka <pawel.k.h1995@gmail.com>
2019-04-11 22:27:27 +0200

    Niepoprawnie działająca instrukcja warunkowa.

--- src/ticket/machine/moneyInsert.java
+++ src/ticket/machine/moneyInsert.java
@@ -8,16 +8,14 @@ public class moneyInsert {
         Scanner inputValue = new Scanner(System.in);
         System.out.println("Wrzuć monety");
         this.value = inputValue.nextDouble();
-        if(this.value != 0.2 || this.value != 0.5 || this.value != 1.0 || this.value != 2.0 || this.value != 5.0 || this.value != 10|| this.value != 20){
-//            do{
+        if(this.value != 0.2 && this.value != 0.5 && this.value != 1.0 && this.value != 2.0 && this.value != 5.0 && this.value != 10 && this.value != 20){
+            do{
                 System.out.println("Błędny nomiał.");
                 System.out.println("Wprowadź dopuszczalny nominał");
                 Scanner inputValue1 = new Scanner(System.in);
                 this.value = inputValue1.nextDouble();
-//                break;
-//            }
-//            while(this.value >= 0 );
-
+            }
+            while(this.value >= 0 );
         }
 
     }
