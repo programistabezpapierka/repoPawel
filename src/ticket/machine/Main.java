e85ff25ff9b5d04820beccbe187db42d950f52dc
Paweł Kózka <pawel.k.h1995@gmail.com>
2019-04-19 23:48:40 +0200

    . Kolejna wersja

--- src/ticket/machine/Main.java
+++ src/ticket/machine/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Ticket ticket1 = new Ticket( 2, "Bilet normalny", 3.5);
+        Ticket ticket1 = new Ticket(1, "Bilet normalny", 3.5);
         Ticket ticket2 = new Ticket(2, "Bilet ulgowy", 1.5);
         Ticket ticket3 = new Ticket(3, "Bilet godzinowy", 3.7);
         Ticket ticket4 = new Ticket(4, "Bilet dobowy", 10.5);
